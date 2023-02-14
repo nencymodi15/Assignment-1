8c842bc2debf1c3acdc627d606abec82cc06a437
Nency Modi <nencymodi150@gmail.com>
2023-02-13 21:50:10 -0500

    The Media Queris for responsiveness

---
+++ public/Script/script.js
@@ -0,0 +1,6 @@
+function toggle(){
+    var nav= document.getElementById("main-navigation");
+    var menu =document.querySelector(".menuul");
+    menu.classList.toggle("show");
+    nav.classList.toggle("navshow");
+}
